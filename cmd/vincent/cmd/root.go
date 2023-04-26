0d5da29603c21d9d1238557657a040a42146b14d
rsteube <rsteube@users.noreply.github.com>
2023-04-26 20:10:56 +0200

    marhalling

--- cmd/vincent/cmd/root.go
+++ cmd/vincent/cmd/root.go
@@ -1,16 +1,40 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/vincent"
 	"github.com/rsteube/vincent/pkg/ui"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "vincent",
 	Short: "terminal color theme chooser",
-	Run: func(cmd *cobra.Command, args []string) {
-		ui.Execute()
+	Args:  cobra.MaximumNArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		switch len(args) {
+		case 0:
+			if cmd.Flag("list").Changed {
+				for _, theme := range vincent.Themes() {
+					print(cmd.OutOrStdout(), theme, "render")
+				}
+			} else {
+				ui.Execute()
+			}
+
+		case 1:
+			print(cmd.OutOrStdout(), args[0], "render")
+
+		case 2:
+			print(cmd.OutOrStdout(), args[0], args[1])
+		}
+
+		return nil
 	},
 	CompletionOptions: cobra.CompletionOptions{
 		DisableDefaultCmd: true,
@@ -22,6 +46,40 @@ func Execute(version string) error {
 	return rootCmd.Execute()
 }
 
+func print(w io.Writer, theme, format string) error {
+	t, err := vincent.Load(theme)
+	if err != nil {
+		return err
+	}
+
+	switch format {
+	case "render":
+		fmt.Fprintln(w, t.Name)
+		fmt.Fprintln(w, t.Render())
+	case "json":
+		m, err := json.Marshal(t)
+		if err != nil {
+			return err
+		}
+		fmt.Println(w, string(m))
+
+	case "yaml":
+		m, err := yaml.Marshal(t)
+		if err != nil {
+			return err
+		}
+		fmt.Println(w, string(m))
+	}
+	return nil
+}
+
 func init() {
-	carapace.Gen(rootCmd)
+	rootCmd.Flags().BoolP("list", "l", false, "list themes")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+			return carapace.ActionValues(vincent.Themes()...)
+		}),
+		carapace.ActionValues("json", "yaml"),
+	)
 }
