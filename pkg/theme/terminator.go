51be941ddaca745d005270f1b629309fbd50d6fa
rsteube <rsteube@users.noreply.github.com>
2023-04-28 12:15:45 +0200

    added terminator

---
+++ pkg/theme/terminator.go
@@ -0,0 +1,40 @@
+package theme
+
+import (
+	"fmt"
+	"strings"
+)
+
+func init() {
+	outputs["terminator"] = func(t Theme) (string, error) {
+		return fmt.Sprintf(`[[%v]]
+    palette = "%v"
+    background_color = "%v"
+    foreground_color = "%v"
+    cursor_color = "%v"`,
+			t.Name,
+			strings.Join([]string{
+				t.Black,
+				t.Red,
+				t.Green,
+				t.Yellow,
+				t.Blue,
+				t.Magenta,
+				t.Cyan,
+				t.White,
+
+				t.BrightBlack,
+				t.BrightRed,
+				t.BrightGreen,
+				t.BrightYellow,
+				t.BrightBlue,
+				t.BrightMagenta,
+				t.BrightCyan,
+				t.BrightWhite}, ":"),
+
+			t.Background,
+			t.Foreground,
+
+			t.Cursor), nil
+	}
+}
