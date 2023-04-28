d01609ca68093fef4ddebbbb03fe28219eadab56
rsteube <rsteube@users.noreply.github.com>
2023-04-28 13:01:45 +0200

    added st

---
+++ pkg/theme/st.go
@@ -0,0 +1,66 @@
+package theme
+
+import "fmt"
+
+func init() {
+	outputs["st"] = func(t Theme) (string, error) {
+		return fmt.Sprintf(`/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+
+	/* 8 bright colors */
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+	"%v",
+
+[256] = "%v", /* default foreground colour */
+[257] = "%v", /* default background colour */
+[258] = "%v", /*575268*/
+
+};
+
+
+/*
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;`,
+			t.Black,
+			t.Red,
+			t.Green,
+			t.Yellow,
+			t.Blue,
+			t.Magenta,
+			t.Cyan,
+			t.White,
+
+			t.BrightBlack,
+			t.BrightRed,
+			t.BrightGreen,
+			t.BrightYellow,
+			t.BrightBlue,
+			t.BrightMagenta,
+			t.BrightCyan,
+			t.BrightWhite,
+
+			t.Foreground,
+			t.Background,
+			t.Cursor,
+		), nil
+	}
+}
