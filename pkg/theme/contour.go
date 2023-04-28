6a9f315ac077748ab4b3b78f2e5bf04537dfae71
rsteube <rsteube@users.noreply.github.com>
2023-04-28 15:34:20 +0200

    added contour

---
+++ pkg/theme/contour.go
@@ -0,0 +1,59 @@
+package theme
+
+import "fmt"
+
+func init() {
+	outputs["contour"] = func(t Theme) (string, error) {
+		return fmt.Sprintf(`color_schemes:
+  %v:
+    normal:
+      black: "%v"
+      red: "%v"
+      green: "%v"
+      yellow: "%v"
+      blue: "%v"
+      magenta: "%v"
+      cyan: "%v"
+      white: "%v"
+
+    bright:
+      black: "%v"
+      red: "%v"
+      green: "%v"
+      yellow: "%v"
+      blue: "%v"
+      magenta: "%v"
+      cyan: "%v"
+      white: "%v"
+
+    default:
+      background: "%v"
+      foreground: "%v"
+
+    cursor:
+      default: "%v"`,
+			t.Name,
+			t.Black,
+			t.Red,
+			t.Green,
+			t.Yellow,
+			t.Blue,
+			t.Magenta,
+			t.Cyan,
+			t.White,
+
+			t.BrightBlack,
+			t.BrightRed,
+			t.BrightGreen,
+			t.BrightYellow,
+			t.BrightBlue,
+			t.BrightMagenta,
+			t.BrightCyan,
+			t.BrightWhite,
+
+			t.Background,
+			t.Foreground,
+
+			t.Cursor), nil
+	}
+}
