50ea377c0200fb72a8d1105f39b8d766693dbd02
rsteube <rsteube@users.noreply.github.com>
2025-09-21 11:36:39 +0200

    updated go

--- cmd/vincent/main.go
+++ cmd/vincent/main.go
@@ -4,7 +4,7 @@ import "github.com/rsteube/vincent/cmd/vincent/cmd"
 
 var version = "develop"
 
-// go:generate go run ../../generate/gen.go
+//go:generate go run ../../generate/gen.go
 func main() {
 	cmd.Execute(version)
 }
